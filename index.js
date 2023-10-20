2fd455b45baea1ac5f3fc385bd86e3a70ab201a9
nji pejkins <njipekins@gmail.com>
2023-10-20 15:09:11 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const  but1 = document.getElementById("para1");
+const  but2 = document.getElementById("para2");
+const  but3 = document.getElementById("para3");
+const  but4 = document.getElementById("para4");
+but1.addEventListener("click",(e)=>{
+    document.getElementById("first").innerHTML=" "
+})
+but2.addEventListener("click",(e)=>{
+    document.getElementById("second").innerHTML=" "
+})
+but3.addEventListener("click",(e)=>{
+    document.getElementById("third").innerHTML=" "
+})
+but4.addEventListener("click",(e)=>{
+    document.getElementById("fourth").innerHTML=" "
+})
